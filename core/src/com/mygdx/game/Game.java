cca4c233550b43e5fa03e14da1d1b84174091db5
Andre <curi_asa@hotmail.com>
2021-05-10 15:26:32 -0300

    Initial commit

---
+++ core/src/com/mygdx/game/Game.java
@@ -0,0 +1,47 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Game extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture passaro;
+	Texture fundo;
+
+	private float larguraDispositivo;
+	private float alturaDispositivo;
+
+	private int movimentaY = 0;
+	private int movimentaX = 0;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		fundo = new Texture("fundo.png");
+		passaro = new Texture("passaro1.png");
+
+		larguraDispositivo = Gdx.graphics.getWidth();
+		alturaDispositivo = Gdx.graphics.getHeight();
+
+	}
+
+	@Override
+	public void render () {
+		batch.begin();
+
+		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+		batch.draw(passaro, movimentaX, movimentaY);
+
+		movimentaX++;
+		movimentaY++;
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
