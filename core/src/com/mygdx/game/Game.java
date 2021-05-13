a07a9525963d64af91ebc4c82c33cc3fc8f3a17b
Andre <curi_asa@hotmail.com>
2021-05-13 19:30:02 -0300

    adicionando alguns comentarios.

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -8,25 +8,34 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
 	//Variaveis globais//
-	SpriteBatch batch;
-	Texture passaro;
-	Texture fundo;
+	private SpriteBatch batch;
+	private Texture[] passaros;
+	private Texture fundo;
+	private Texture canoTopo;
+	private Texture canoBaixo;
 
 	private float larguraDispositivo;
 	private float alturaDispositivo;
-
 	private int movimentaY = 0;
 	private int movimentaX = 0;
+
+	private float variacao = 0;
+	private  float gravidade = 0;
+	private  float posicaoInicialVerticalPassaro = 0;
 	
 	@Override
 	public void create () {
 		//Criando objetos em cena//
 		batch = new SpriteBatch();
+		passaros = new Texture[3];
+		passaros[0] = new Texture("passaro1.png");
+		passaros[1] = new Texture("passaro2.png");
+		passaros[2] = new Texture("passaro3.png");
 		fundo = new Texture("fundo.png");
-		passaro = new Texture("passaro1.png");
 
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo = Gdx.graphics.getHeight();
+		posicaoInicialVerticalPassaro = alturaDispositivo / 2;
 
 	}
 
@@ -34,12 +43,27 @@ public class Game extends ApplicationAdapter {
 
 	public void render () {
 		batch.begin();
+		//adicionando variação entre animações//
+		if(variacao > 3)
+			variacao = 0;
+
+		//adicionando regra de botão ao tocar na tela//
+		boolean toqueTela = Gdx.input.justTouched();
+		if(Gdx.input.justTouched()){
+			gravidade = -25;
+		}
+
+		if(posicaoInicialVerticalPassaro > 0 || toqueTela)
+			posicaoInicialVerticalPassaro = posicaoInicialVerticalPassaro - gravidade;
 
 		//Definindo posição e formato dos objetos//
 		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
-		batch.draw(passaro, movimentaX, movimentaY);
+		batch.draw(passaros[(int)variacao],30,posicaoInicialVerticalPassaro);
 
-		//adicionando movimentação//
+		variacao += Gdx.graphics.getDeltaTime() * 10;
+
+		//adicionando movimentação e fisica//
+		gravidade++;
 		movimentaX++;
 		movimentaY++;
 		batch.end();
